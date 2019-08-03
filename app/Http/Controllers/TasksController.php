7e5b51f9108ad1d0cad08014c641262fdff954d2
kazumasa.yamanishi <baboon@addas.jp>
2019-08-03 03:22:59 +0000

    Lesson13 課題 tasklistアプリ

---
+++ app/Http/Controllers/TasksController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Task;
+
+class TasksController extends Controller
+{
+    // get で tasks/ にアクセスされた場合の「一覧表示処理」
+    public function index()
+    {
+        $tasks = Task::all();
+        
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+
+    // get で tasks/create/ にアクセスされた場合の「新規登録画面表示処理」
+    public function create()
+    {
+        $task = new Task;
+        
+        return view('tasks.create', [
+            'task' => $task,
+        ]);
+    }
+
+    // post で tasks/ にアクセスされた場合の「新規登録処理」
+    public function store(Request $request)
+    {
+        $task = new Task;
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    // get で tasks/id/ にアクセスされた場合の「取得表示処理」
+    public function show($id)
+    {
+        $task = Task::find($id);
+        
+        return view('tasks.show', [
+            'task' => $task,
+        ]);
+    }
+
+    // get で tasks/id/edit にアクセスされた場合の「更新画面表示処理」
+    public function edit($id)
+    {
+        $task = Task::find($id);
+        
+        return view('tasks.edit', [
+            'task' => $task,
+        ]);
+    }
+
+    // put または patch で tasks/id にアクセスされた場合の「更新処理」
+    public function update(Request $request, $id)
+    {
+        $task = Task::find($id);
+        $task->content = $request->content;
+        $task->save();
+        
+        return redirect('/');
+    }
+
+    // delete で tasks/id にアクセスされた場合の「削除処理」
+    public function destroy($id)
+    {
+        $task = Task::find($id);
+        $task->delete();
+        
+        return redirect('/');
+    }
+}
